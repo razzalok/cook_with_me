33fedd95b62320f2063a488a0adada0a0116ca1c
Alok Raj <50761757+razzalok@users.noreply.github.com>
2023-06-11 08:38:24 +0530

    input err

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,27 +6,24 @@ const Main = () => {
  const [meals,setMeals] = useState({})
  const baseUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
 
-  const handleSubmit = (event) => {
-      event.preventDefault();
-      // console.log(meal);
-      setMeal(meal)
-    }
+//  const handleChange =(e)=>{
+//   setMeal(e.target.value);
+//   onChange={handleChange}
+//  }
+const handleSubmit = (e) => {
+  e.preventDefault()
+  setMeal(e.target.value);
+  setMeal('')
+}
     
 
   const fetchDetails =  useCallback(async()=>{
   const {data}= await Axios.get(baseUrl+meal)
     console.log(data);
-    // const meal = data.meals[0]
-
-    // console.log(data.meals[0]);
   setMeals(data.meals[0])
   },[meal])
  
   fetchDetails()
-
-  // useEffect(() => {
-  //   fetchDetails();
-  // }, [fetchDetails]);
 
   return (
     <div >
@@ -34,7 +31,7 @@ const Main = () => {
       <p className='bg-cyan-900 py-5 flex-initial flex-auto w-[40%] text-5xl text-[#CAD5E2] font-extrabold text-center w-full md:w-auto'>Cook With Me</p>
 
       <form onSubmit={handleSubmit} className='flex-initial text-center mt-10 ' >
-            <input className='rounded-lg border-2 w-[40%] h-10 p-6 bg-[#758283] text-[#fff] text-[20px] border-none ' type="text" name='meal' value={meal} onChange={(e) => setMeal(e.target.value)} />
+            <input className='rounded-lg border-2 w-[40%] h-10 p-6 bg-[#758283] text-[#fff] text-[20px] border-none ' type="text" name='meal' value={meal}  />
             <input className='rounded-lg border-2  ml-2 p-2 bg-[#207398] text-[#fff] text-[20px] border-none  hover:bg-[#03203C] ease-in duration-300' id='search' type="submit" />
         </form>
         <Hero meals={meals}/>
